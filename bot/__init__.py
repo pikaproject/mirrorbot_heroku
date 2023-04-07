776857127fab10440c81183c685eea0d9a01106f
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-04-07 20:03:40 +0800

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -666,6 +666,8 @@ UPDATE_PACKAGES = environ.get('UPDATE_PACKAGES', '')
 if len(UPDATE_PACKAGES) == 0:
     UPDATE_PACKAGES = ''
 
+SAFE_MODE = environ.get('false')
+
 
 config_dict = {'ANILIST_ENABLED': ANILIST_ENABLED,
                'AS_DOCUMENT': AS_DOCUMENT,
